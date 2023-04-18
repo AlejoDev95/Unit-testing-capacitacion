82b4eb7a052fe185f37d7fa7ae71c09d8c0b5dfb
Julian Sanchez <alejo15san95@gmail.com>
2023-04-17 22:35:58 -0500

    Test for the calculator class

--- src/app/feature/calculator/calculator.ts
+++ src/app/feature/calculator/calculator.ts
@@ -3,7 +3,12 @@ import { Observable, of } from 'rxjs';
 export class Calculator {
   public addNumbers = (num1: number, num2: number): number => num1 + num2;
 
-  public divideNumbers = (num1: number, num2: number): number => num1 / num2;
+  public divideNumbers = (num1: number, num2: number): number | null => {
+    if (num2 === 0) {
+      return null;
+    }
+    return num1 / num2;
+  };
 
   public async subtractNumbers(num1: number, num2: number): Promise<number> {
     return num1 - num2;
